b986526314a17f120d16c54d007c003551af8ef8
takeshi-komori <stubgurssy@gmail.com>
2022-08-06 10:47:15 +0900

    組み込み関数実装

--- ruby/interpreter/interpreter.rb
+++ ruby/interpreter/interpreter.rb
@@ -1,51 +1,77 @@
 require "./minruby"
 
-def evaluate(tree, env)
+# サンプル組み込み関数
+def sample_add(x, y)
+    x + y
+end
+
+def evaluate(tree, genv, lenv)
 
     case tree[0]
     when "lit"
         tree[1]
     when "+"
-        evaluate(tree[1], env) + evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) + evaluate(tree[2], genv, lenv)
     when "-"
-        evaluate(tree[1], env) - evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) - evaluate(tree[2], genv, lenv)
     when "*"
-        evaluate(tree[1], env) * evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) * evaluate(tree[2], genv, lenv)
     when "/"
-        evaluate(tree[1], env) / evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) / evaluate(tree[2], genv, lenv)
     when "%"
-        evaluate(tree[1], env) % evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) % evaluate(tree[2], genv, lenv)
     when "**"
-        evaluate(tree[1], env) ** evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) ** evaluate(tree[2], genv, lenv)
     when "<"
-        evaluate(tree[1], env) < evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) < evaluate(tree[2], genv, lenv)
+    when "<="
+        evaluate(tree[1], genv, lenv) <= evaluate(tree[2], genv, lenv)
     when ">"
-        evaluate(tree[1], env) > evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) > evaluate(tree[2], genv, lenv)
+    when ">="
+        evaluate(tree[1], genv, lenv) >= evaluate(tree[2], genv, lenv)
     when "=="
-        evaluate(tree[1], env) == evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) == evaluate(tree[2], genv, lenv)
     when "!="
-        evaluate(tree[1], env) != evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) != evaluate(tree[2], genv, lenv)
     when "func_call"
-        p(evaluate(tree[2], env))
-    when "var_assign"
-        env[tree[1]] = evaluate(tree[2], env)
-    when "var_ref"
-        env[tree[1]]
-    when "if"
-        if evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        args = []
+        i = 0
+        while tree[2 + i] != nil
+            args[i] = evaluate(tree[2 + i], genv, lenv)
+            i = i + 1
+        end
+
+        method = genv[tree[1]]
+        if method[0] == "buildin"
+            # 組み込み関数
+            minruby_call(method[1], args)
         else
-            evaluate(tree[3], env)
+
+        end
+    when "var_assign"
+        lenv[tree[1]] = evaluate(tree[2], genv, lenv)
+    when "var_ref"
+        lenv[tree[1]]
+    when "if"
+        if evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
+        else
+            evaluate(tree[3], genv, lenv)
         end
     when "while"
-        while evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        while evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
+        end
+    when "while2"
+        while evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
         end
     when "stmts"
         i = 1
         last = nil
         while tree[i] != nil
-            last = evaluate(tree[i], env)
+            last = evaluate(tree[i], genv, lenv)
             i = i + 1
         end
         last
@@ -60,5 +86,9 @@ tree = minruby_parse(str)
 pp(tree)
 
 # 構文木を解析し、結果を出力
-env = {}
-answer = evaluate(tree, env)
+genv = {
+    "p" => ["buildin", "p"],
+    "add" => ["buildin", "sample_add"]
+}
+lenv = {}
+answer = evaluate(tree, genv, lenv)
