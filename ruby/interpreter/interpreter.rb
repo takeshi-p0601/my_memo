12fb12dd02f7495a846085d6e9ac7b11981fda94
takeshi-komori <stubgurssy@gmail.com>
2022-08-06 09:27:57 +0900

    条件分岐の実装あたりまで

--- ruby/interpreter/interpreter.rb
+++ ruby/interpreter/interpreter.rb
@@ -1,6 +1,7 @@
 require "./minruby"
 
 def evaluate(tree, env)
+
     case tree[0]
     when "lit"
         tree[1]
@@ -22,12 +23,24 @@ def evaluate(tree, env)
         evaluate(tree[1], env) > evaluate(tree[2], env)
     when "=="
         evaluate(tree[1], env) == evaluate(tree[2], env)
+    when "!="
+        evaluate(tree[1], env) != evaluate(tree[2], env)
     when "func_call"
         p(evaluate(tree[2], env))
     when "var_assign"
         env[tree[1]] = evaluate(tree[2], env)
     when "var_ref"
         env[tree[1]]
+    when "if"
+        if evaluate(tree[1], env)
+            evaluate(tree[2], env)
+        else
+            evaluate(tree[3], env)
+        end
+    when "while"
+        while evaluate(tree[1], env)
+            evaluate(tree[2], env)
+        end
     when "stmts"
         i = 1
         last = nil
