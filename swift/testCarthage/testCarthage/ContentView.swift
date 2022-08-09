101fcf4ea1686073867c7acb7e44f78527064c3c
takeshi-1000 <stubgurssy@gmail.com>
2022-08-10 06:46:34 +0900

    add carthange project

---
+++ swift/testCarthage/testCarthage/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  testCarthage
+//
+//  Created by Takeshi Komori on 2022/08/09.
+//
+
+import SwiftUI
+import Lottie
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/testCarthage/testCarthage/testCarthageApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCarthageApp.swift
+//  testCarthage
+//
+//  Created by Takeshi Komori on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct testCarthageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
